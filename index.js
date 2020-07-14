9de453ff69e37a6b62052d36f96859e9bae6cc4e
h-krank <68291059+h-krank@users.noreply.github.com>
2020-07-14 13:19:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var cronometro = (function(){
+
+    var display = document.getElementById('temporizador');
+    var i = 0;
+    var segundos = 0;
+    var minutos = 0;
+    var horas = 0;
+    var parado = true
+
+
+    function iniciar(){
+        //Não permite ter mais de um intervalo
+        if (parado){
+            parado = false
+            interval = setInterval(contador, 1000);
+        }
+    }
+
+    function parar(){
+        parado = true
+        clearInterval(interval);
+    }
+
+    function zerar(){
+        parar()
+        i = -1; //Retorna o contador para 0
+        
+        contador() //Atualiza display
+    }
+
+    function contador(){
+        i++
+        segundos = i;
+        
+        //Converte segundos totais em horas/minutos/segundos
+        horas = Math.floor(segundos / 3600)
+        minutos = Math.floor(segundos % 3600 / 60)
+        segundos = (segundos % 3600 % 60)
+
+        display.textContent =  `${String(horas).padStart(2,'0')}:${String(minutos).padStart(2,'0')}:${String(segundos).padStart(2,'0')}`;
+    }
+
+    return {
+        iniciar,
+        parar,
+        zerar,
+    }
+
+})()
